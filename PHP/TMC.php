5a99258fa574990f43717c0c3431a516e47bfb7f
Giuseppe <toxiicpp@gmail.com>
2017-10-10 17:40:08 +0200

    Update

--- PHP/TMC.php
+++ PHP/TMC.php
@@ -1,12 +1,8 @@
 <?php
-
 /* Temporary Email Checker
-
 Written by: toxiicdev.net
 Language: PHP
-
 */
-
 interface TMCResponseCodes
 {
 	const NotDisposable = 1;
@@ -23,20 +19,20 @@ class TMC
 			$ch = curl_init(); 
 			curl_setopt($ch, CURLOPT_URL, "https://services.toxiicdev.net/tmc/check.php?email=$email"); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-			curl_setopt($cHandler, CURLOPT_SSL_VERIFYHOST, false);
-			curl_setopt($cHandler, CURLOPT_SSL_VERIFYPEER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			$output = curl_exec($ch);
-			curl_close($ch);
 			
 			if(!curl_errno($ch)) // If all went fine
 			{
 				$json = json_decode($output);
-				return $json['result_code'];
+				return $json -> result_code;
 			}
+			
+			curl_close($ch);
 		}
 		
 		return TMCResponseCodes::Invalid;
 	}
 }
-
 ?>
